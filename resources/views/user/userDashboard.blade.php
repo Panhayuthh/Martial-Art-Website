d6d360c1def4e442df85cd6bc520abefb4125fc6
phanreach <reach4phan@gmail.com>
2024-12-09 17:00:03 +0700

    config route

--- resources/views/user/userDashboard.blade.php
+++ resources/views/user/userDashboard.blade.php
@@ -7,6 +7,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
+  @section('title','User Dashboard')
+ 
   <x-layout>
     <!-- Hero Section -->
     <div class="position-relative bg-dark text-white">
@@ -230,6 +232,7 @@
     </div>
   </div>
 </x-layout>
+<x-footer></x-footer>
 </body>
 </html>
 
